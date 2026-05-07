7c47c315e0e96eee6bc432e9f36d7958dd258b06
Lukemansahid <lukemansahid2020@gmail.com>
2026-05-07 11:15:04 +0000

    Demo on each team mate to colabo

--- ucampt-frontend/src/main.jsx
+++ ucampt-frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// <button> Save student info</button>
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
